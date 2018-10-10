8e2232304dd3d18f8f2a80aa165e92b0e8823ca4
Navgurukul025 <ranjeet18@navgurukul.org>
2018-10-10 18:01:08 +0530

    first commit

---
+++ ra.py
@@ -0,0 +1,7 @@
+speed = int(raw_input ("gaadi ki speed daalo>"))
+if speed <= 30:
+    print "Speed bahot kam hai"
+elif speed <= 60:
+    print "Speed kam hai"
+elif speed >= 100:
+    print "Speed bahot fast hai"
---
+++ test.py
@@ -0,0 +1,12 @@
+variable = raw_input('Please enter name: ')
+
+if variable == 'vishal':   
+	print('Ye condition True ho gayi hain') # Ye tab print hoga jab if wali condition True hogi
+elif variable == 'vikrant':
+	print('vikrant print ho raha hai')
+elif variable == "kajal":
+ 	print ("kajal kaise ho tum")
+elif variable =="mamta":
+	print "kya kar rahi ho"
+else:
+	print('Ye condition False ho gayi hai') # ye tab print hoga jab condition False Hogi
